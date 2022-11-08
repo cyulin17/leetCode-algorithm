4d7f6a38d1c93d46898ac4006df381d8d0b73625
ChiaYu Lin <lin.1008@osu.edu>
2022-11-08 02:01:24 +0000

    Add solution to Reverse Linked List from LeetCode.

---
+++ js/reverseList.js
@@ -0,0 +1,13 @@
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  let next = null;
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+reverseList([1, 2]);
